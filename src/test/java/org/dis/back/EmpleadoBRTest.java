525bc220faab990ebbbb57c6496296cc9955a3c6
Nacho Serrano <ignaserrano@gmail.com>
2020-12-31 00:35:59 +0100

    Adding correct tests folder

---
+++ src/test/java/org/dis/back/EmpleadoBRTest.java
@@ -0,0 +1,27 @@
+package org.dis.back;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class EmpleadoBRTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void calculaSalarioBruto() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void calculaSalarioNeto() {
+    }
+}
